ac5fbb9943826368e6d2605af7af780b5a42a924
natecordova <natecordovaortiz@hotmail.com>
2019-08-01 12:11:39 -0500

    Cargar animales localmente y random

--- Assets/Scripts/NuevoCambiarImagen.cs
+++ Assets/Scripts/NuevoCambiarImagen.cs
@@ -7,17 +7,93 @@ using UnityEngine.UI;
 
 public class NuevoCambiarImagen : MonoBehaviour
 {
+
+    public Image anim1, anim2, anim3, anim4;
+    private db objdb;
+
+    private string monoBase, rataBase, osoBase, venadoBase;
+
+
+    string[] rutas = {
+         "https://raw.githubusercontent.com/natecordova/mibosque/master/Assets/Sprites/mono%20capuchino_1.png",
+        "https://raw.githubusercontent.com/natecordova/mibosque/master/Assets/Sprites/rata%20espinosa_1.png",
+        "https://raw.githubusercontent.com/natecordova/mibosque/master/Assets/Sprites/oso%20perezoso_1.png",
+        "https://raw.githubusercontent.com/natecordova/mibosque/master/Assets/Sprites/venado%20cola%20blanca-01.png"
+    };
+
+
     // The output of the image
     public Image img;
 
     // The source image
-    public string url = "https://clipground.com/images/monkey-baby-clipart-14.jpg";
+     string url = "https://clipground.com/images/monkey-baby-clipart-14.jpg";
 
     IEnumerator Start()
     {
-        WWW www = new WWW(url);
+
+        
+
+
+        //Asignar cada game object
+        anim1 = GameObject.Find("Mono").GetComponent<Image>();
+        anim2 = GameObject.Find("Raton").GetComponent<Image>();
+        anim3 = GameObject.Find("Oso").GetComponent<Image>();
+        anim4 = GameObject.Find("Venado").GetComponent<Image>();
+
+
+        //Cargar las imagenes locales
+        //objdb.sqlite_consulta("SELECT * FROM especie WHERE id = '2'");
+
+        objdb = new db();
+
+        Debug.Log("CONSULTA" + objdb.sqlite_consulta("SELECT * FROM especie WHERE id = '1'"));
+
+        monoBase = objdb.sqlite_consulta("SELECT * FROM especie WHERE id = '1'");
+        rataBase = objdb.sqlite_consulta("SELECT * FROM especie WHERE id = '2'");
+        osoBase = objdb.sqlite_consulta("SELECT * FROM especie WHERE id = '3'");
+        venadoBase = objdb.sqlite_consulta("SELECT * FROM especie WHERE id = '4'");
+
+
+
+        anim1.sprite = Resources.Load<Sprite>(monoBase); //"Sprites/oso perezoso_1"
+        anim2.sprite = Resources.Load<Sprite>(rataBase); //"Sprites/rata espinosa_1"
+        anim3.sprite = Resources.Load<Sprite>(osoBase); //"Sprites/mono capuchino_1"
+        anim4.sprite = Resources.Load<Sprite>(venadoBase); //"Sprites/venado cola blanca-01"
+
+        Debug.Log("monooo" + monoBase);
+
+        //  Debug.Log("anim sql" + objdb.reader.GetString(3));
+
+
+        // Generate a random index less than the size of the array.  
+        System.Random rand = new System.Random();
+        int index = rand.Next(rutas.Length);
+
+
+        //Cargar imágenes desde url
+        WWW www = new WWW((rutas[index]));
         yield return www;
         img.sprite = Sprite.Create(www.texture, new Rect(0, 0, www.texture.width, www.texture.height), new Vector2(0, 0), 1);
+    }
+
+    public void Update()
+    {
+
+
+        // anim1.sprite = Resources.Load<Sprite>("Sprites/mono capuchinoNegro");  //Cargar en ejecucion el animal
+
+
+
+
+
+
+
+
+
+
+        //Método con www cargar imagenes
+        //WWW anim1_tex = new WWW("Sprites/mono capuchinoNegro");
+        //anim1.sprite = Sprite.Create(anim1_tex.texture, new Rect(0, 0, anim1.sprite.rect.width,anim1.sprite.rect.height), new Vector2(0, 0), 1); 
     }
 
 
---
+++ Assets/Scripts/db.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mono.Data.Sqlite;
+using System.Data;
+using System;
+
+public class db : MonoBehaviour
+{
+
+
+    //public GameObject img;
+    
+    
+
+
+
+    private void Start()
+    {
+
+
+        sqlite_conexion();
+
+
+
+    }
+
+
+   
+
+
+     void sqlite_conexion()
+    {
+        string conn = "URI=file:" + Application.dataPath + "/Plugins/mibosque.db"; //Path to database.
+        IDbConnection dbconn;
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open(); //Open connection to the database.
+
+        IDbCommand dbcmd = dbconn.CreateCommand(); //Variable que almacena la consulta a la base
+        string sqlQuery = "SELECT * FROM especie";
+        dbcmd.CommandText = sqlQuery;
+        IDataReader reader = dbcmd.ExecuteReader();
+
+        while (reader.Read())
+        {
+            int id = reader.GetInt32(0);   // (0) es el primer valor en el array del primer campo de la base (id)
+            string nombre = reader.GetString(1);   // (1) es el segundo valor en el array del segundo campo de la base (nombre)
+            string tipo = reader.GetString(2);
+            string url = reader.GetString(3);
+
+            //int rand = reader.GetInt32(2);
+
+            Debug.Log("id= " + id + "  nombre =" + nombre + "  tipo =" + tipo + "  url =" + url);
+        }
+
+        reader.Close();
+        reader = null;
+
+        dbcmd.Dispose();
+        dbcmd = null;
+
+        dbconn.Close();
+        dbconn = null;
+    }
+
+    //funcion que recibe una sentencia sql
+    // y regresa la url 
+
+
+    public string sqlite_consulta(String consulta)
+    {
+        string conn = "URI=file:" + Application.dataPath + "/Plugins/mibosque.db"; //Path to database.
+        IDbConnection dbconn;
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open(); //Open connection to the database.
+
+        IDbCommand dbcmd = dbconn.CreateCommand(); //Variable que almacena la consulta a la base
+        string sqlQuery = consulta;
+        dbcmd.CommandText = sqlQuery;
+        IDataReader reader = dbcmd.ExecuteReader();
+
+        while (reader.Read())
+        {
+            int id = reader.GetInt32(0);   // (0) es el primer valor en el array del primer campo de la base (id)
+            string nombre = reader.GetString(1);   // (1) es el segundo valor en el array del segundo campo de la base (nombre)
+            string tipo = reader.GetString(2);
+            string url = reader.GetString(3);
+            string rutaLocal = reader.GetString(4);
+
+            consulta = rutaLocal;  //Asigna la ruta guardada en la base a la consulta que se devuelve
+            Debug.Log("La consulta " + consulta);
+
+            //int rand = reader.GetInt32(2);
+
+            Debug.Log("xxxid= " + id + "  nombre =" + nombre + "  tipo =" + tipo + "  url =" + url + "  rutaLocal =" + rutaLocal);
+        }
+
+        reader.Close();
+        reader = null;
+
+        dbcmd.Dispose();
+        dbcmd = null;
+
+        dbconn.Close();
+        dbconn = null;
+
+
+       
+
+
+        return consulta;
+    }
+
+
+
+
+}
+
+
+
+
